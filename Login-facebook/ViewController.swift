d6bb01e81db55ec3561ff7869ae7cfd64a97fbdc
Arturo bm <arturo.bravo@axity.com>
2020-04-22 14:41:06 -0500

    Finaliza la contrucción de la vista de login facebook

--- Login-facebook/ViewController.swift
+++ Login-facebook/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var headerImageView: UIImageView!
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var loginDataView: UIView!
+    @IBOutlet weak var loginLineDataView: UIView!
+    @IBOutlet weak var userTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var initSessionButton: UIButton!
+    @IBOutlet weak var forgotenPasswordButton: UIButton!
+    @IBOutlet weak var createAccountButton: UIButton!
+    @IBOutlet weak var buttonLineView: UIView!
+    @IBOutlet weak var orLabel: UILabel!
+    
+    private let primaryColor = UIColor(displayP3Red: 56/255, green: 117/255, blue: 233/255, alpha: 1)
+    private let secondaryColor = UIColor(displayP3Red: 173/255, green: 202/255, blue: 250/255, alpha: 1)
+    private let tertiaryColor = UIColor(displayP3Red: 254/255, green: 254/255, blue: 254/255, alpha: 1)
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        logoImageView.layer.cornerRadius = logoImageView.bounds.height / 2
+        logoImageView.clipsToBounds = true
+        
+        
     }
 
 
